153b2c17c2e3f77b941fb00d133c557f82cf1a86
Mirodeon <mehdi.habaru@gmail.com>
2023-06-30 09:14:31 +0200

    feat(ImgAvatar): Add ing Avatar, roundView function, DarkGreen color in assets

--- EViOS1/ViewController.swift
+++ EViOS1/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ImgAvatar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setBgImg(img: UIImage(named: "background_image")!, view: view)
+        setImgAvatar(
+            img: UIImage(named: "user_icon")!,
+            bg: UIColor(named: "DarkGreen")!,
+            view: ImgAvatar
+        )
     }
 
     func setBgImg(img: UIImage, view: UIView){
@@ -22,6 +28,17 @@ class ViewController: UIViewController {
         imageView.center = view.center
         view.addSubview(imageView)
         self.view.sendSubviewToBack(imageView)
+    }
+    
+    func roundView(view:UIView){
+        view.layer.cornerRadius = view.frame.size.height/2
+    }
+    
+    func setImgAvatar(img: UIImage, bg: UIColor, view: UIImageView){
+        view.image = img
+        view.contentMode = .scaleAspectFit
+        view.backgroundColor = bg
+        roundView(view: view)
     }
 }
 
